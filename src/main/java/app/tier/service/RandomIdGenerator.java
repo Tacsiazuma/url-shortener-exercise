144a3630994c3ddca2eb7c5fc8570f4a2616fc48
Krisztian Papp <fejlesztes@letscode.hu>
2023-01-19 16:20:50 +0100

    Added some explanation on random ids

--- src/main/java/app/tier/service/RandomIdGenerator.java
+++ src/main/java/app/tier/service/RandomIdGenerator.java
@@ -6,6 +6,9 @@ import java.util.Random;
 public class RandomIdGenerator {
 
     // 58 because we exclude misreaded characters like 0/O, I/l from the original base62
+    // still have a quite high possibilities approx. 38 billion
+    // if we assume a million daily active users, 10% of which creates a new url every day
+    // 365*0.1m = 36.5 million new entries a year.
     private static final int CHARACTER_POSSIBILITIES = 58;
 
     private static final char[] BASE_58_CHARS = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz"
